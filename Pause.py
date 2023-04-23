678fa60e833b609637a8e5af308ddfe9e651a41e
YuriProduction <jurexda@yandex.ru>
2023-04-23 21:19:12 +0500

    Added pause button

---
+++ Pause.py
@@ -0,0 +1,16 @@
+import pygame
+import sys
+
+
+class PauseButton():
+    def __init__(self):
+        self.rect = pygame.Rect(10, 10, 30, 40)
+
+    def draw(self, screen):
+        WHITE = (255, 255, 255)
+        GREEN = (0, 255, 0)
+        # создаем кнопку
+        pausebutton = pygame.Rect(10, 10, 30, 40)
+        pygame.draw.rect(screen, GREEN, pausebutton)
+        pygame.draw.line(screen, WHITE, (20, 20), (20, 40), 5)
+        pygame.draw.line(screen, WHITE, (30, 20), (30, 40), 5)
